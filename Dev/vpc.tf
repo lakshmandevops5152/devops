d4609ea39d1e0273a67498dc5276f3598ae46b49
lakshmandevops5152 <malapatilakshman@gmail.com>
2025-09-19 15:10:06 +0530

    Update vpc.tf

--- Dev/vpc.tf
+++ Dev/vpc.tf
@@ -29,21 +29,4 @@ resource "aws_internet_gateway" "Devops-igw" {
   }
 }
 
-resource "aws_route_table" "demo_rt" {
-  vpc_id = aws_vpc.Devops.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.Devops-igw.id
-  }
-
-  tags = {
-    Name = "example"
-  }
-}
-
-resource "aws_route_table_association" "demo_rt_association" {
-  subnet_id      = aws_subnet.demo_1.id
-  route_table_id = aws_route_table.demo_rt.id
-}
 
