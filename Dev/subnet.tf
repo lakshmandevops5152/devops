e9d5a78e440782b453bb2247f00b28603ec1b275
lakshmandevops5152 <malapatilakshman@gmail.com>
2025-09-19 17:40:49 +0530

    Created subnet.tf

---
+++ Dev/subnet.tf
@@ -0,0 +1,25 @@
+
+
+resource "aws_subnet" "Devops-2" {
+    vpc_id = aws_vpc.demo.id
+    cidr_block = "192.168.2.0/24"
+    availability_zone = "ap-south-1b"
+    map_public_ip_on_launch = true
+
+    tags = {
+      Name = "Devops-subnet -2" 
+    }
+  
+}
+resource "aws_subnet" "Devops-3" {
+    vpc_id = aws_vpc.demo.id
+    cidr_block = "192.168.3.0/24"
+    availability_zone = "ap-south-1b"
+    map_public_ip_on_launch = true
+
+    tags = {
+      Name = "Devops-subnet -3" 
+    }
+  
+}
+
