140c6317bacd08a1ffc7cadf2922385caf5907a0
lakshmandevops5152 <malapatilakshman@gmail.com>
2025-09-19 18:17:07 +0530

    Update subnet.tf

--- Dev/subnet.tf
+++ Dev/subnet.tf
@@ -1,35 +1,32 @@
+resource "aws_subnet" "Devops_1" {
+  vpc_id                  = aws_vpc.Devops.id
+  cidr_block              = "192.168.1.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
 
-
-resource "aws_subnet" "Devops-1" {
-    vpc_id =  aws_vpc.Devops.id
-    cidr_block = "192.168.2.0/24"
-    availability_zone = "ap-south-1b"
-    map_public_ip_on_launch = true
-
-    tags = {
-      Name = "Devops-subnet -1" 
-    }
-  
-}
-resource "aws_subnet" "Devops-2" {
-    vpc_id =  aws_vpc.Devops.id
-    cidr_block = "192.168.2.0/24"
-    availability_zone = "ap-south-1b"
-    map_public_ip_on_launch = true
-
-    tags = {
-      Name = "Devops-subnet -2" 
-    }
-  
-resource "aws_subnet" "Devops-3" {
-    vpc_id = aws_vpc.Devops.id
-    cidr_block = "192.168.3.0/24"
-    availability_zone = "ap-south-1b"
-    map_public_ip_on_launch = true
-
-    tags = {
-      Name = "Devops-subnet -3" 
-    }
-  
+  tags = {
+    Name = "Devops-subnet-1"
+  }
 }
 
+resource "aws_subnet" "Devops_2" {
+  vpc_id                  = aws_vpc.Devops.id
+  cidr_block              = "192.168.2.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Devops-subnet-2"
+  }
+}
+
+resource "aws_subnet" "Devops_3" {
+  vpc_id                  = aws_vpc.Devops.id
+  cidr_block              = "192.168.3.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Devops-subnet-3"
+  }
+}
