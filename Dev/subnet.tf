bedf8a6477806e0f035e40b501e82b63f9ccc34e
lakshmandevops5152 <malapatilakshman@gmail.com>
2025-09-19 17:59:51 +0530

    Update subnet.tf

--- Dev/subnet.tf
+++ Dev/subnet.tf
@@ -1,7 +1,7 @@
 
 
 resource "aws_subnet" "Devops-2" {
-    vpc_id = aws_vpc.demo.id
+    vpc_id =  aws_vpc.Devops.id
     cidr_block = "192.168.2.0/24"
     availability_zone = "ap-south-1b"
     map_public_ip_on_launch = true
@@ -12,7 +12,7 @@ resource "aws_subnet" "Devops-2" {
   
 }
 resource "aws_subnet" "Devops-3" {
-    vpc_id = aws_vpc..id
+    vpc_id = aws_vpc.Devops.id
     cidr_block = "192.168.3.0/24"
     availability_zone = "ap-south-1b"
     map_public_ip_on_launch = true
