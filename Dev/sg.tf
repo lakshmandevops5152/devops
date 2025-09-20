56dce46023d3ea3f64f7971e85f2c107b7b4e7f5
lakshmandevops5152 <malapatilakshman@gmail.com>
2025-09-20 13:09:30 +0530

    Update sg.tf

--- Dev/sg.tf
+++ Dev/sg.tf
@@ -1,1 +1,29 @@
 
+resource "aws_security_group" "Devops-sg-1" {
+  name        = "Jenkins"
+  description = "This sg for Jenkins instance"
+  vpc_id      = aws_vpc.Devops.id
+
+  tags = {
+    Name = "Jenkins"
+  }
+}
+
+
+resource "aws_security_group_rule" "inboud_allo_all_port" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.Devops-sg-1.id
+}
+
+resource "aws_security_group_rule" "allow_all" {
+  type              = "egress"
+  to_port           = 0
+  protocol          = "-1"
+  from_port         = 0
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.Devops-sg-1.id
+}
